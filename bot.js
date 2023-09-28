81f8c8762123c474bfc06063173c19e6c49cb388
souheil <souheil.yaz@gmail.com>
2023-09-27 21:45:39 -0400

    chore(bot): format

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Client,Events, GatewayIntentBits} = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const OpenAIApi = require("openai");
 const dotenv = require('dotenv');
 
@@ -8,7 +8,7 @@ const intentList = [
     GatewayIntentBits.MessageContent,
 ]
 
-const client = new Client({intents: intentList});
+const client = new Client({ intents: intentList });
 const bot_token = 'YOUR_DISCORD_BOT_TOKEN'
 
 dotenv.config();
@@ -22,30 +22,30 @@ client.once(Events.ClientReady, c => {
 
 client.login(bot_token);
 
-client.on('message', async(message) => {
+client.on('message', async (message) => {
     if (message.content.startsWith('!dalle')) {
         const query = message.content.slice(7); // remove "!dalle" prefix
         const generatedImage = await generateImage(query);
-        message.channel.send({files: [generatedImage]});
+        message.channel.send({ files: [generatedImage] });
     }
 })
 
 async function generateImage(query) {
     try {
-        const response = await openai.createImage ({
+        const response = await openai.createImage({
             prompt: query,
             n: 1,
             model: 'dalle'
         });
-    
 
-    const imageId = response.id;
-    const imageUrl = response.output.url;
 
-    const imageResponse = await openai.retrieveImage(imageId);
+        const imageId = response.id;
+        const imageUrl = response.output.url;
 
-    const attachment = new Discord.MessageAttachment(imageResponse, 'generated_image.png');
-    return attachment;
+        const imageResponse = await openai.retrieveImage(imageId);
+
+        const attachment = new Discord.MessageAttachment(imageResponse, 'generated_image.png');
+        return attachment;
     } catch (error) {
         console.error('An error occurred while generating the image:', error);
         throw new Error('Image generation failed. Please try again later.');
